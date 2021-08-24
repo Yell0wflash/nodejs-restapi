0d87d5f118cc8409519de7dc6f821d2808fb9edb
Yell0wFlash <davidbus1450@gmail.com>
2021-08-24 21:25:43 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 const { cekKey } = require('../database/db'); 
 const { youtubePlay, youtubeMp4, youtubeMp3 } = require('../controllers/yt');
+const { asupan } = require('../controllers/asupantiktok')
 const { cakLontong, bijak, quotes, fakta, ptl, motivasi } = require('../controllers/randomtext');
 
 router.get('/checkkey', async (req, res) => {
@@ -35,5 +36,7 @@ router.get('/bijak', bijak);
 router.get('/ptl', ptl);
 
 router.get('/motivasi', motivasi);
+
+router.get('/asupan', asupan);
 
 module.exports = router;
