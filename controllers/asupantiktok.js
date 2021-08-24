8a5ffca918d79e51a7778df6056fbc6057dfeadf
Yell0wFlash <davidbus1450@gmail.com>
2021-08-24 21:22:30 +0700

    Create asupantiktok.js

---
+++ controllers/asupantiktok.js
@@ -0,0 +1,25 @@
+const skrep = require("../lib/skrep");
+const { cekKey } = require('../database/db');
+
+async function youtubePlay(req, res) {
+    const query = req.query.query;
+    const apikey = req.query.apikey;
+    if (query === undefined || apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter query & apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    skrep.asupantiktok(query).then(result => {
+        res.status(200).send({status: 200, result: result});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error'
+        })
+    });
+}
