4e4a5435237b2a28995b048228fbb85376de5412
Yell0wFlash <davidbus1450@gmail.com>
2021-08-25 14:20:09 +0700

    Update fitur.js

--- controllers/fitur.js
+++ controllers/fitur.js
@@ -89,3 +89,69 @@ exports.army = async(req, res) => {
         })
     });
 }
+exports.glowing = async(req, res) => {
+    const query = req.query.query;
+    const apikey = req.query.apikey;
+    if (query === undefined || apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter query & apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    skrep.textmakervid(query, 'glowing').then(resu => {
+        res.status(200).send({status: 200, result: resu.url});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error'
+        })
+    });
+}
+exports.retro = async(req, res) => {
+    const query = req.query.query;
+    const apikey = req.query.apikey;
+    if (query === undefined || apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter query & apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    skrep.textmakervid(query, 'retro').then(resu => {
+        res.status(200).send({status: 200, result: resu.url});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error'
+        })
+    });
+}
+exports.bold = async(req, res) => {
+    const query = req.query.query;
+    const apikey = req.query.apikey;
+    if (query === undefined || apikey === undefined) return res.status(404).send({
+        status: 404,
+        message: `Input Parameter query & apikey`
+    });
+    const check = await cekKey(apikey);
+    if (!check) return res.status(403).send({
+        status: 403,
+        message: `apikey ${apikey} not found, please register first!`
+    });
+    skrep.textmakervid(query, 'bold').then(resu => {
+        res.status(200).send({status: 200, result: resu.url});
+    }).catch(error => {
+        console.log(error);
+        res.status(500).send({
+            status: 500,
+            message: 'Internal Server Error'
+        })
+    });
+}
